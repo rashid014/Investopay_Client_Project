b0026bb6f3549f18fa7cd8bcd5f14e6feeb61f72
rashid014 <rh701164@gmail.com>
2024-07-01 20:42:25 +0530

    emailjs updated

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,16 +1,48 @@
-"use client"
+"use client";
 import React, { useState } from 'react';
 import { Card } from 'antd';
 import Front from '../../../public/assets/Front3.jpg';
 import Image from 'next/image';
-import AnimatedHeader from '../TitleText/title-text'
+import emailjs from 'emailjs-com';
+import AnimatedHeader from '../TitleText/title-text';
 
 interface FormProps {}
 
 const Form: React.FC<FormProps> = () => {
-  const [handleSubmit, setHandleSubmit] = useState<React.FormEventHandler<HTMLFormElement>>(() => (event:any) => {
-    event.preventDefault();
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
+    country: 'US',
   });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    emailjs.send(process.env.NEXT_PUBLIC_SERVICE_ID??'', process.env.NEXT_PUBLIC_TEMPLATE_ID??'', formData, process.env.NEXT_PUBLIC_USER_ID??'')
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      }, (error) => {
+        console.log('FAILED...', error);
+      });
+  };
 
   return (
     <div className="flex flex-wrap justify-center bg-white">
@@ -29,25 +61,49 @@ const Form: React.FC<FormProps> = () => {
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
-          </div>
+        </div>
         <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
             <div>
               <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">First name</label>
               <div className="mt-2.5">
-                <input type="text" name="first-name" id="first-name" autoComplete="given-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input
+                  type="text"
+                  name="firstName"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                  value={formData.firstName}
+                />
               </div>
             </div>
             <div>
               <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
               <div className="mt-2.5">
-                <input type="text" name="last-name" id="last-name" autoComplete="family-name" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input
+                  type="text"
+                  name="lastName"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                  value={formData.lastName}
+                />
               </div>
             </div>
             <div className="sm:col-span-2">
               <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
               <div className="mt-2.5">
-                <input type="email" name="email" id="email" autoComplete="email" className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                  value={formData.email}
+                />
               </div>
             </div>
             <div className="sm:col-span-2">
@@ -55,22 +111,43 @@ const Form: React.FC<FormProps> = () => {
               <div className="relative mt-2.5">
                 <div className="absolute inset-y-0 left-0 flex items-center">
                   <label htmlFor="country" className="sr-only">Country</label>
-                  <select id="country" name="country" className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm">
-                    <option>US</option>
-                    <option>CA</option>
-                    <option>EU</option>
+                  <select
+                    id="country"
+                    name="country"
+                    className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                    onChange={handleSelectChange}
+                    value={formData.country}
+                  >
+                    <option value="US">US</option>
+                    <option value="CA">CA</option>
+                    <option value="EU">EU</option>
                   </select>
                   <svg className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <input type="tel" name="phone-number" id="phone-number" autoComplete="tel" className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
+                <input
+                  type="tel"
+                  name="phoneNumber"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                  value={formData.phoneNumber}
+                />
               </div>
             </div>
             <div className="sm:col-span-2">
               <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
               <div className="mt-2.5">
-                <textarea name="message" id="message" rows={4} className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                <textarea
+                  name="message"
+                  id="message"
+                  rows={4}
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  onChange={handleInputChange}
+                  value={formData.message}
+                ></textarea>
               </div>
             </div>
             <div className="flex gap-x-4 sm:col-span-2">
