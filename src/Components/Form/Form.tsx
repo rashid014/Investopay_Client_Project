2ba1ee939acdc5c2b9b286cb5f852f6c8440505e
rashid014 <rh701164@gmail.com>
2024-11-22 22:47:46 +0530

    messages added

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,26 +1,28 @@
 "use client";
-import React, { useState } from 'react';
-import { Card } from 'antd';
-import Front from '../../../public/assets/Front3.jpg';
-import Image from 'next/image';
-import emailjs from 'emailjs-com';
-import AnimatedHeader from '../TitleText/title-text';
+import React, { useState } from "react";
+import { Card } from "antd";
+import Front from "../../../public/assets/Front3.jpg";
+import Image from "next/image";
+import emailjs from "emailjs-com";
 
 interface FormProps {}
 
 const Form: React.FC<FormProps> = () => {
   const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    message: '',
-    country: 'IND',
+    firstName: "",
+    lastName: "",
+    email: "",
+    phoneNumber: "",
+    message: "",
+    country: "IND",
   });
+
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [isFailure, setIsFailure] = useState(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     const { name, value } = e.target;
-    setFormData(prevFormData => ({
+    setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
@@ -28,7 +30,7 @@ const Form: React.FC<FormProps> = () => {
 
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
-    setFormData(prevFormData => ({
+    setFormData((prevFormData) => ({
       ...prevFormData,
       [name]: value,
     }));
@@ -36,12 +38,26 @@ const Form: React.FC<FormProps> = () => {
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    emailjs.send(process.env.NEXT_PUBLIC_SERVICE_ID??'', process.env.NEXT_PUBLIC_TEMPLATE_ID??'', formData, process.env.NEXT_PUBLIC_USER_ID??'')
-      .then((response) => {
-        console.log('SUCCESS!', response.status, response.text);
-      }, (error) => {
-        console.log('FAILED...', error);
-      });
+    setIsSuccess(false); // Reset state on new submission
+    setIsFailure(false);
+
+    emailjs
+      .send(
+        process.env.NEXT_PUBLIC_SERVICE_ID ?? "",
+        process.env.NEXT_PUBLIC_TEMPLATE_ID ?? "",
+        formData,
+        process.env.NEXT_PUBLIC_USER_ID ?? ""
+      )
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          setIsSuccess(true);
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          setIsFailure(true);
+        }
+      );
   };
 
   return (
@@ -49,23 +65,35 @@ const Form: React.FC<FormProps> = () => {
       <Card className="w-[700px] bg-white lg:mt-48">
         <Image
           src={Front}
-          style={{ width: '100%', height: 'auto', maxWidth: '100%', maxHeight: '100%' }}
+          style={{ width: "100%", height: "auto", maxWidth: "100%", maxHeight: "100%" }}
           alt="My Image"
         />
-        {/* <AnimatedHeader/> */}
       </Card>
 
       <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8 flex-1">
-        <div className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]" aria-hidden="true">
-          <div className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]" style={{ clipPath: 'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)' }}></div>
+        <div
+          className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+          aria-hidden="true"
+        >
+          <div
+            className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+            style={{
+              clipPath:
+                "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+          ></div>
         </div>
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Contact sales</h2>
         </div>
         <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
+          {/* Form fields */}
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            {/* First Name */}
             <div>
-              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+              <label htmlFor="first-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                First name
+              </label>
               <div className="mt-2.5">
                 <input
                   type="text"
@@ -78,8 +106,11 @@ const Form: React.FC<FormProps> = () => {
                 />
               </div>
             </div>
+            {/* Last Name */}
             <div>
-              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+              <label htmlFor="last-name" className="block text-sm font-semibold leading-6 text-gray-900">
+                Last name
+              </label>
               <div className="mt-2.5">
                 <input
                   type="text"
@@ -92,8 +123,11 @@ const Form: React.FC<FormProps> = () => {
                 />
               </div>
             </div>
+            {/* Email */}
             <div className="sm:col-span-2">
-              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+              <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">
+                Email
+              </label>
               <div className="mt-2.5">
                 <input
                   type="email"
@@ -106,11 +140,16 @@ const Form: React.FC<FormProps> = () => {
                 />
               </div>
             </div>
+            {/* Phone Number */}
             <div className="sm:col-span-2">
-              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
+              <label htmlFor="phone-number" className="block text-sm font-semibold leading-6 text-gray-900">
+                Phone number
+              </label>
               <div className="relative mt-2.5">
                 <div className="absolute inset-y-0 left-0 flex items-center">
-                  <label htmlFor="country" className="sr-only">Country</label>
+                  <label htmlFor="country" className="sr-only">
+                    Country
+                  </label>
                   <select
                     id="country"
                     name="country"
@@ -118,13 +157,19 @@ const Form: React.FC<FormProps> = () => {
                     onChange={handleSelectChange}
                     value={formData.country}
                   >
-                    <option value="US">US</option>
-                    <option value="CA">CA</option>
-                    <option value="EU">EU</option>
-                    <option value="EU">IND</option>
+                    <option>IND</option>
                   </select>
-                  <svg className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                    <path fillRule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clipRule="evenodd" />
+                  <svg
+                    className="pointer-events-none absolute right-3 top-0 h-full w-5 text-gray-400"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                      clipRule="evenodd"
+                    />
                   </svg>
                 </div>
                 <input
@@ -138,8 +183,11 @@ const Form: React.FC<FormProps> = () => {
                 />
               </div>
             </div>
+            {/* Message */}
             <div className="sm:col-span-2">
-              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+              <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">
+                Message
+              </label>
               <div className="mt-2.5">
                 <textarea
                   name="message"
@@ -151,23 +199,22 @@ const Form: React.FC<FormProps> = () => {
                 ></textarea>
               </div>
             </div>
-            <div className="flex gap-x-4 sm:col-span-2">
-              <div className="flex h-6 items-center">
-                <button type="button" className="bg-gray-200 flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600" role="switch" aria-checked="false" aria-labelledby="switch-1-label">
-                  <span className="sr-only">Agree to policies</span>
-                  <span aria-hidden="true" className="translate-x-0 h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"></span>
-                </button>
-              </div>
-              <label className="text-sm leading-6 text-gray-600" id="switch-1-label">
-                By selecting this, you agree to our
-                <a href="#" className="font-semibold text-indigo-600">privacy&nbsp;policy</a>.
-              </label>
-            </div>
           </div>
+          {/* Submit Button */}
           <div className="mt-10">
-            <button type="submit" className="block w-full rounded-md bg-blue-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let&apos;s talk</button>
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Let&apos;s talk
+            </button>
           </div>
         </form>
+        {/* Conditional Messages */}
+        <div className="mt-6 text-center">
+          {isSuccess && <p className="text-green-500">Your message was sent successfully!</p>}
+          {isFailure && <p className="text-red-500">Something went wrong. Please try again later.</p>}
+        </div>
       </div>
     </div>
   );
